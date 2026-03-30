0c1b356b9ec0b4d6eda9cc71d2086c708e7d14c7
cause-dev <cause.dev@proton.me>
2026-03-30 22:47:18 +0500

    setup compose.yaml file and add postgres database

--- app/monitors/admin.py
+++ app/monitors/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Monitor
 
 # Register your models here.
+
+@admin.register(Monitor)
+class MonitorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'url', 'expected_status_code', 'created_at')
+    list_filter = ('expected_status_code', 'created_at')
+    search_fields = ('name', 'url')
+    ordering = ('-created_at',)
--- app/monitors/models.py
+++ app/monitors/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Monitor(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="monitors")
+    name = models.CharField(max_length=100)
+    url = models.URLField()
+    expected_status_code = models.PositiveIntegerField(default=200)
+    is_active = models.BooleanField(default=True)
+    last_checked = models.DateTimeField(null=True, blank=True)
+    is_online = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.url})"
